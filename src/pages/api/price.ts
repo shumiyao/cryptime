f452c124b11530d07ece565ad77012c5ce554da1
Shu Miyao <shu.andrade.miyao@gmail.com>
2023-10-27 01:07:07 +0100

    oops

--- src/pages/api/price.ts
+++ src/pages/api/price.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 const CoinMarketCap = require('coinmarketcap-api')
+const NEXT_PUBLIC_CMC_API_KEY = process.env.NEXT_PUBLIC_CMC_API_KEY;
 //  /v1/cryptocurrency/listings/latest
 type Data = {
   name: string
@@ -10,7 +11,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const apiKey = '5aca907e-890f-4711-8709-9627eb48dc8b'
+  const apiKey = NEXT_PUBLIC_CMC_API_KEY
   const client = new CoinMarketCap(apiKey)
   const quote = await client.getQuotes({ symbol: 'BTC', convert: 'USD' }).then(e => e.data.BTC.quote.USD).catch(e => e)
 
